8853c12d77841cdb8e7809480d37b32f90560294
jjhangu <jjhangu@gmail.com>
2016-03-08 16:59:44 +0900

    commit

--- heapsort.py
+++ heapsort.py
@@ -1,6 +1,7 @@
 import math
 
 arr = [4, 10, 3, 5, 1]
+result = [];
 
 def swap(arr, i, j):
     temp = arr[i]
@@ -10,7 +11,7 @@ def swap(arr, i, j):
 def createMaxHeap(arr):
     print(arr);
 
-    startIndex = math.floor(len(arr)//2);
+    startIndex = math.floor(len(arr)//2)-1;
     while startIndex > -1:
         maxHeapify(arr, startIndex)
         startIndex = startIndex-1
@@ -25,15 +26,26 @@ def maxHeapify(arr, startIndex):
         largest = left
 
     if right < len(arr) and  arr[startIndex] < arr[right]:
-        largest = right
+        if arr[left] < arr[right]:
+            largest = right
 
     if largest != startIndex:
-        if arr[left] >  arr[right]:
-            largest =left
-        else:
-            largest = right
+
         swap(arr, startIndex, largest)
         maxHeapify(arr, largest)
 
-createMaxHeap(arr)
-print(arr)
+def heapsort (result, arr):
+    while len(arr)>0:
+        createMaxHeap(arr)
+        print("complete maxheap")
+        print(arr)
+        ''' result 노드에 추가 '''
+        result.append(arr[0])
+        '''마지막 노드와 첫번째 노드 교체'''
+        if len(arr) != 1:
+            arr[0] = arr[len(arr)-1]
+        arr.pop()
+
+heapsort(result, arr)
+
+print(result)
