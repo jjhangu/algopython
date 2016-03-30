de7f0eb3d2271dfdeea9b642c5f0e382b0b365c2
sanghwa jung <jjhangu@gmail.com>
2016-03-30 11:12:58 +0900

    Update disjoint-set-naive.py

--- greedyalgorithm/disjoint-set-naive.py
+++ greedyalgorithm/disjoint-set-naive.py
@@ -26,11 +26,14 @@ sets = list(map(lambda x:MakeSet(x), range(10)))
 for i in range(len(sets)):
     print(sets[i].number, sets[i].parent)
 
+# 0-4까지 한개의 셋트를만든다
 for i in range(0, 4):
     union(sets[i], sets[i+1])
 
+# 5- 마지막까지 한개의 셋트를 만든다
 for i in range(5, len(sets)-1):
     union(sets[i], sets[i+1])
 
+# 모든 노드의 루트 노드찾기 테스트
 for i in range(len(sets)):
     print(i ," parent -> ", find(sets[i]).number)
